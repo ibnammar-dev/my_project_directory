bc90485a8faf1a31b5a939e7102e0450234c01cc
ibnammar-dev <ibnammar.dev@gmail.com>
2025-10-12 15:54:22 +0100

    setting up bootstrap

--- src/Service/PostService/PostService.php
+++ src/Service/PostService/PostService.php
@@ -2,65 +2,132 @@
 
 namespace App\Service\PostService;
 
-use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Post;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Validator\Exception\ValidationFailedException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use App\Repository\PostRepository;
+use Doctrine\DBAL\Exception as DBALException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PostService
 {
-    private EntityManagerInterface $entityManager;
-    private ValidatorInterface $validator;
-    private PostRepository $postRepository;
     public function __construct(
-        EntityManagerInterface $entityManager,
-        ValidatorInterface $validator,
-        PostRepository $postRepository, // Inject the repository to avoid circular dependency
-    ) {
-        $this->entityManager = $entityManager;
-        $this->validator = $validator;
-        $this->postRepository = $postRepository;
-    }
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ValidatorInterface $validator,
+        private readonly PostRepository $postRepository
+    ) {}
 
+    /**
+     * Get all posts from the database.
+     *
+     * @return Post[]
+     */
     public function getAllPosts(): array
     {
         return $this->postRepository->findAll();
     }
 
+    /**
+     * Get a post by its ID.
+     *
+     * @param int $id The post ID
+     * @return Post The post entity
+     * @throws NotFoundHttpException When post is not found
+     */
     public function getPostById(int $id): Post
     {
         $post = $this->postRepository->find($id);
+
         if (!$post) {
-            throw new NotFoundHttpException('Post with id ' . $id . ' not found');
+            throw new NotFoundHttpException(sprintf('Post with id %d not found', $id));
         }
+
         return $post;
     }
 
-    public function createPost(Post $post): void
+    /**
+     * Create a new post.
+     *
+     * @param Post $post The post entity to create
+     * @return Post The created post entity
+     * @throws ValidationFailedException When validation fails
+     * @throws DBALException When database operation fails
+     */
+    public function createPost(Post $post): Post
     {
         $this->validatePost($post);
-        $this->entityManager->persist($post);
-        $this->entityManager->flush();
+
+        $this->entityManager->beginTransaction();
+
+        try {
+            $this->entityManager->persist($post);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+
+            return $post;
+        } catch (DBALException $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 
-    public function updatePost(Post $post): void
+    /**
+     * Update an existing post.
+     *
+     * @param Post $post The post entity to update
+     * @return Post The updated post entity
+     * @throws ValidationFailedException When validation fails
+     * @throws DBALException When database operation fails
+     */
+    public function updatePost(Post $post): Post
     {
         $this->validatePost($post);
-        $this->entityManager->persist($post);
-        $this->entityManager->flush();
+
+        $this->entityManager->beginTransaction();
+
+        try {
+            $this->entityManager->persist($post);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+
+            return $post;
+        } catch (DBALException $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 
+    /**
+     * Delete a post.
+     *
+     * @param Post $post The post entity to delete
+     * @throws DBALException When database operation fails
+     */
     public function deletePost(Post $post): void
     {
-        $this->entityManager->remove($post);
-        $this->entityManager->flush();
+        $this->entityManager->beginTransaction();
+
+        try {
+            $this->entityManager->remove($post);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (DBALException $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 
-    public function validatePost(Post $post): void
+    /**
+     * Validate a post entity.
+     *
+     * @param Post $post The post entity to validate
+     * @throws ValidationFailedException When validation fails
+     */
+    private function validatePost(Post $post): void
     {
         $errors = $this->validator->validate($post);
+
         if (count($errors) > 0) {
             throw new ValidationFailedException($post, $errors);
         }
